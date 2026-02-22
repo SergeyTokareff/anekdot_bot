b4e0aacbd81be4d23600304e94d43b18ee269546
SergeyTokareff <sergey.v.tokareff@gmail.com>
2026-02-22 13:41:17 +0200

    fix

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
 import asyncio
-from bot.handlers import dp, bot
+from fastapi import FastAPI, Request
+from aiogram import Bot
+from aiogram.types import Update
+from bot.handlers import dp
+from bot.config import TELEGRAM_TOKEN
+
+app = FastAPI()
+
+bot = Bot(token=TELEGRAM_TOKEN)
+
+WEBHOOK_PATH = "/webhook"
+WEBHOOK_SECRET = "supersecret"
+WEBHOOK_URL = f"https://anekdot-bot-qraq.onrender.com{WEBHOOK_PATH}"
 
 
-async def main():
-    print("Bot started...")
-    await dp.start_polling(bot)
+@app.on_event("startup")
+async def on_startup():
+    await bot.set_webhook(
+        url=WEBHOOK_URL,
+        secret_token=WEBHOOK_SECRET
+    )
+    print("Webhook set!")
 
 
-if __name__ == "__main__":
-    asyncio.run(main())
+@app.on_event("shutdown")
+async def on_shutdown():
+    await bot.delete_webhook()
+    await bot.session.close()
+
+
+@app.post(WEBHOOK_PATH)
+async def webhook_handler(request: Request):
+    if request.headers.get("X-Telegram-Bot-Api-Secret-Token") != WEBHOOK_SECRET:
+        return {"status": "forbidden"}
+
+    data = await request.json()
+    update = Update.model_validate(data)
+    await dp.feed_update(bot, update)
+    return {"status": "ok"}
